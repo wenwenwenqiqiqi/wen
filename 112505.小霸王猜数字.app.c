ec7c0535974782404047e1deec0f1207e62d7fc1
wenwenwenqiqiqi <74357843+wenwenwenqiqiqi@users.noreply.github.com>
2020-11-25 17:48:22 +0800

    Create 112505.小霸王猜数字.app.c

---
+++ 112505.小霸王猜数字.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int i=54;
+	int a;
+	printf("友情提示：输入1-100的数字哦~\n");
+	while(scanf("%d",&a))
+	if(a==i)
+	{printf("恭喜你猜对啦");}
+	else if(a<i)
+	{printf("太小了哦");}
+	else if(a>i)
+	{printf("大了哦");}
+
+}

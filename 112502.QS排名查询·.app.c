45a72a19505284577cd4389fe8497bc662631cea
wenwenwenqiqiqi <74357843+wenwenwenqiqiqi@users.noreply.github.com>
2020-11-25 09:53:23 +0800

    Create 112502.QS排名查询·.app.c

---
+++ 112502.QS排名查询·.app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+int a;
+scanf("%d",&a);
+if(a==1)
+{printf("麻省理工大学");}
+else if(a==2)
+{printf("斯坦福大学");}
+else if(a==3)
+{printf("哈佛大学");}
+else if(a==4)
+{printf("牛津大学");}
+else if(a==5)
+{printf("加州理工大学");}
+else if(a==6)
+{printf("苏黎世联邦理工大学");}
+else if(a==7)
+{printf("剑桥大学");}
+else if(a==8)
+{printf("伦敦大学学院");}
+else if(a==9)
+{printf("帝国理工学院");}
+else if(a==10)
+{printf("芝加哥大学");}
+else
+{printf("老铁的排名太靠后了，请输入1-10");}
+}

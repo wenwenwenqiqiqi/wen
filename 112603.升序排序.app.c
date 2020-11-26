e7594bfaed4745eda2ae930d8a96f96fbc0d7189
wenwenwenqiqiqi <74357843+wenwenwenqiqiqi@users.noreply.github.com>
2020-11-26 20:19:57 +0800

    Create 112603.升序排序.app.c

---
+++ 112603.升序排序.app.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,i;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b)
+{i=a;
+a=b;
+b=i;}
+if(a>c){
+i=a;
+a=c;
+c=i;}
+if(b>c)
+{
+i=b;
+b=c;
+c=i;
+}
+printf("%d,%d,%d\n",a,b,c);
+}
